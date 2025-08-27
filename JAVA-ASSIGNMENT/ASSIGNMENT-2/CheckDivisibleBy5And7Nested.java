1bc306025bd995b1ceeaac814c7c3b229550881c
unknown <aniketshinde2027@gmail.com>
2025-08-27 18:06:52 +0530

    Check no Divisible By 5 And 7 use Nested

---
+++ JAVA-ASSIGNMENT/ASSIGNMENT-2/CheckDivisibleBy5And7Nested.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+	public class CheckDivisibleBy5And7Nested{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the No :");
+	int no = sc.nextInt();
+	
+	if(no % 5 ==0)
+	if(no % 7 ==0)
+		System.out.println(no+" is divisible by both 5 and 7");
+	else
+		System.out.println(no+" is divisible by 5 but not 7");
+	else 
+		System.out.println(no+" is not divisible by both 5 and 7");
+	
+}
+}
+
