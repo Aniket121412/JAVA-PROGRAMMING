85170fb6a3396308d78231e41a2068eaf1fe4ef5
unknown <aniketshinde2027@gmail.com>
2025-08-28 13:31:35 +0530

    Compare Two Numbers

---
+++ JAVA-ASSIGNMENT/ASSIGNMENT-2/CompareTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+	public class CompareTwoNumbers{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a :");
+	int a = sc.nextInt();
+	System.out.println("Enter b :");
+	int b = sc.nextInt();
+
+	if(a == b)
+		System.out.println(""+a+" is equal to "+b+"");
+	else if (a < b)
+		System.out.println(""+a+" is less than "+b+"");
+	else
+		System.out.println(""+a+" is greater than "+b+"");
+}
+}
